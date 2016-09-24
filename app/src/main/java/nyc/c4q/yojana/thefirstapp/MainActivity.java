15daaccde1dd5cf02fee0f16db3f0eda13a7aeb6
SharmaYojana <joy.dfm@gmail.com>
2016-09-24 19:22:40 -0400

    thirdd commit

---
+++ app/src/main/java/nyc/c4q/yojana/thefirstapp/MainActivity.java
@@ -0,0 +1,48 @@
+package nyc.c4q.yojana.thefirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static String EXTRA_MESSAGE = "C4Q";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//minimum SDK
+//activity to run codes in android
+//all activities in android has to be defined in manifest
+//handler for navigating
+//clock cycle
+//intent = navigation for activities
+//context= current state of your device
+//anonymous class
